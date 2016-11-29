1ac35e5efbb12a1e166b4973de571f52012f4460
xubojoy <xubojoy1112@163.com>
2016-11-29 14:26:15 +0800

    Initial Commit

---
+++ UIVisualEffectViewStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIVisualEffectViewStudy
+//
+//  Created by xubojoy on 2016/11/29.
+//  Copyright © 2016年 xubojoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIVisualEffectViewStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIVisualEffectViewStudy
+//
+//  Created by xubojoy on 2016/11/29.
+//  Copyright © 2016年 xubojoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
